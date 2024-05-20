2e06cb6fe93f312537f055d22cc3298cce25ead4
Suman <sumanranjan17291729@gmail.com>
2024-05-21 02:34:03 +0530

    first

--- src/Container4/Cards.jsx
+++ src/Container4/Cards.jsx
@@ -9,7 +9,7 @@ export default function Cards() {
               <div className="relative left-[15.8vw] bottom-[-8.6vh] max-sm:hidden">
                 <img src="src\Container4\Carts.png" alt="" />
               </div>
-              {/* <Img src ={el.img}></Img> */}
+              {/* <Img src ={el.img}></Img> */} 
               <img src={el.img} height="400" width="400" />
               <h1 className="mt-[-3rem] font-bold ml-5 text-white">
                 {el.text}
